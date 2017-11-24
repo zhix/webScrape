c48ed8c62f2e4aaf0c9f635a4742269dcb5d171b
zhix <zhix.chong@gmail.com>
2017-11-24 23:52:11 +0800

    zipcode added

--- webScrape2.py
+++ webScrape2.py
@@ -1,4 +1,6 @@
-#!/bin/usr/python3
+#!/usr/bin/env python3
+
+## using this script by running this "sudo python3 webScrape2.py" on terminal
 
 import urllib.request
 from bs4 import BeautifulSoup
@@ -8,9 +10,10 @@ import json
 from pprint import pprint
 import csv
 
-listNum = [35801, 35802, 35803, 35804]
+listNum = list(range(1111, 100000))
+storeIDlist = []
 
-with open("trial.csv", 'w') as myFile: 
+with open("outputStore.csv", 'w') as myFile: 
 	csvFile = csv.writer(myFile, delimiter=',', quoting=csv.QUOTE_ALL)
 	csvFile.writerow(["Name", "StoreID", "City", "County", "PostCode", "Phone", "Manager", "URL"])
 
@@ -28,12 +31,16 @@ with open("trial.csv", 'w') as myFile:
 		json_text = re.search(r'^\s*THD_GLOBAL\.JSON_REP\s*=\s*({.*?})\s*;\s*$',
 			script.string, flags=re.DOTALL | re.MULTILINE).group(1)
 
+		print (json_text)
+
 		## extract only the store information
 		data = json.loads(json_text)['stores']
 
 		# pprint(data[0])
 		j = 0
+
 		while j <len(data): 
+
 			name = data[j]['name']
 			storeid = data[j]['storeId']
 			uri = data[j]['url']
@@ -48,6 +55,12 @@ with open("trial.csv", 'w') as myFile:
 			manager = data[j]['storeContacts'][0]['name']
 
 			dataList = [name, storeid, city, county, postcode, phone, manager, uri]
-			#print(dataList)
-			csvFile.writerow(dataList)
+
+			## check if the row has already existed before writing the row 
+			if storeid in storeIDlist: 
+				print("Store ID: " + storeid + " already exists")
+			else: 
+				storeIDlist.append(storeid)
+				csvFile.writerow(dataList)	
+			
 			j = j+1
---
+++ zipcode.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+## This script outputs the list of zipcodes of US 
+
+zipcodes = []
+
+# zipcodes = zipcodes + list(range(35801, 35816+1)) ##Alabama (AL)	Huntsville	
+# zipcodes = zipcodes + list(range(99501, 99524+1)) ##Alaska (AK)	Anchorage
+
+# zipcodes = zipcodes + list(range(85001, 85055+1)) ##Arizona (AZ)	Phoenix	
+
+# zipcodes = zipcodes + list(range(72201, 72217+1)) ##Arkansas (AR)	Little Rock	
+
+# zipcodes = zipcodes + list(range(94203, 94209+1)) ##California (CA)	Sacramento
+# zipcodes = zipcodes + list(range(90001, 90089+1)) ##California (CA)	Los Angeles
+# zipcodes = zipcodes + list(range(90209, 90213+1)) ##California (CA) Beverly Hills
+
+# zipcodes = zipcodes + list(range(80201, 80239+1)) ##Colorado (CO)	Denver	
+
+# zipcodes = zipcodes + list(range(6101, 6112+1)) ##Conneticut (CT)	Hartford	//should have 0 in front (take note)
+# zipcodes = zipcodes + list(range(19901, 19905+1)) ##Deleware (DE)	Dover	
+# zipcodes = zipcodes + list(range(20001, 20020+1)) ##District of Columbia (DC)	Washington	
+
+# zipcodes = zipcodes + list(range(32501, 32509+1)) ##Florida (FL)	Pensacola
+# zipcodes = zipcodes + list(range(33124, 33190+1)) ##Florida (FL)	Miami
+# zipcodes = zipcodes + list(range(32801, 32837+1)) ##Florida (FL)	Orlando	
+
+# # Georgia (GA)	Atlanta	30301 thru 30381
+# # Hawaii (HI)	Honolulu	96801 thru 96830
+# # Idaho (ID)	Montpelier	83254
+# # Illinois (IL)	Chicago
+ 
+# # Springfield	60601 thru 60641
+ 
+# # 62701 thru 62709
+# # Indiana (IN)	Indianapolis	46201 thru 46209
+# # Iowa (IA)	Davenport
+ 
+# # Des Moines	52801 thru 52809
+ 
+# # 50301 thru 50323
+# # Kansas (KS)	Wichita	67201 thru 67221
+# # Kentucky (KY)	Hazard	41701- 41702
+# # Lousiana (LA)	New Orleans	70112 thru 70119
+# # Maine (ME)	Freeport	04032 thru 04034
+# # Maryland (MD)	Baltimore	21201 thru 21237
+# # Massachusetts (MA)	Boston	02101 thru 02137
+# # Michigan (MI)	Coldwater
+ 
+# # Gaylord	49036
+ 
+# # 49734 - 49735
+# # Minnesota (MN)	Duluth	55801 thru 55808
+# # Mississippo (MS)	Biloxi	39530 thru 39535
+# # Missouri (MO)	St. Louis	63101 thru 63141
+# # Montana (MT)	Laurel	59044
+# # Nebraska (NE)	Hastings	68901 - 68902
+# # Nevada (NV)	Reno	89501 thru 89513
+# # New Hampshire (NH)	Ashland	03217
+# # New Jersey (NJ)	Livingston	07039
+# # New Mexico (NM)	Santa Fe	87500 thru 87506
+# # New York (NY)	New York	10001 thru 10048
+# # North Carolina (NC)	Oxford	27565
+# # North Dakota (ND)	Walhalla	58282
+# # Ohio (OH)	Cleveland	44101 thru 44179
+# # Oklahoma (OK)	Tulsa	74101 thru 74110
+# # Oregon (OR)	Portland	97201 thru 97225
+# # Pennsylvania (PA)	Pittsburgh	15201 thru 15244
+# # Rhode Island (RI)	Newport	02840 - 02841
+# # South Carolina (SC)	Camden	29020
+# # South Dakota (SD)	Aberdeen	57401 - 57402
+# # Tennessee (TN)	Nashville	37201 thru 37222
+# # Texas (TX)	Austin	78701 thru 78705
+# # Utah (UT)	Logan	84321 thru 84323
+# # Vermont (VT)	Killington	05751
+# # Virginia (VA)	Altavista	24517
+# # Washington (WA)	Bellevue (home of windoze)	98004 thru 98009
+# # West Virginia (WV)	Beaver	25813
+# # Wisconsin (WI)	Milwaukee	53201 thru 53228
+# # Wyoming (WY)	Pinedale	82941
+
+
+zipcodes = zipcodes + list(range(1111, 100000))
+print (zipcodes)
